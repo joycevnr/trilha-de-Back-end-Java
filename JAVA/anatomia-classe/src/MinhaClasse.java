ad9722a69befe880c7ef044df73994942b74598a
joycevnr <joycevitoria150@gmail.com>
2024-05-30 21:07:35 -0300

    my class

---
+++ JAVA/anatomia-classe/src/MinhaClasse.java
@@ -0,0 +1,13 @@
+
+//classe com o mesmo nome do arquivo
+//Classes: PascalCase (e.g., MinhaClasse)
+//Métodos e Variáveis: camelCase (e.g., meuMetodo, minhaVariavel)
+public class MinhaClasse{ //classe executável tem o método main: tem a capacidade de realizar uma inicialização do projeto
+    
+    public static void main (String [] args) {
+        System.out.print ("Olá Mundo");
+
+        final String BR = "Brasil"; //final- variável que não é alterável, a variável toda em maiúscula nunca é alterada
+ 
+    }
+}
