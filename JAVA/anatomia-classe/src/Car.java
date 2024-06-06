12a926f98253ab7c4abccd70d3e3b125cc77a209
joycevnr <joycevitoria150@gmail.com>
2024-06-05 23:16:49 -0300

    simple example

---
+++ JAVA/anatomia-classe/src/Car.java
@@ -0,0 +1,23 @@
+//simple example 
+
+public class Car {
+
+    // Attributes
+    int year;
+    String color;
+
+    // Method (sub-routine)
+    public void accelerate() {
+        System.out.println("The car is accelerating.");
+    }
+
+    public void brake() {
+        System.out.println("The car is braking.");
+    }
+
+    public void honk() {
+        System.out.println("The car is honking.");
+    }
+}
+
+
