669513df843a109b81f4feef0d38711fa1067e7c
joycevnr <joycevitoria150@gmail.com>
2024-06-05 23:32:38 -0300

    ExemploMetodos

---
+++ JAVA/anatomia-classe/src/ExemploMetodos.java
@@ -0,0 +1,22 @@
+public class ExemploMetodos {
+    
+    // Método estático
+    public static void metodoEstatico() {
+        System.out.println("Este é um método estático.");
+    }
+    
+    // Método não estático (ou de instância)
+    public void metodoNaoEstatico() {
+        System.out.println("Este é um método não estático.");
+    }
+    
+    public static void main(String[] args) {
+        // Chamando o método estático diretamente pela classe
+        ExemploMetodos.metodoEstatico();
+        
+        // Criando uma instância da classe para chamar o método não estático
+        ExemploMetodos exemplo = new ExemploMetodos();
+        exemplo.metodoNaoEstatico();
+    }
+}
+
