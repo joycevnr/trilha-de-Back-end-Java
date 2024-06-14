b93848c76ae82310e2dc7280f4e299a26d2c55e9
joycevnr <joycevitoria150@gmail.com>
2024-06-14 12:59:09 -0300

    desafio contaTerminal

---
+++ JAVA/contaBanco/src/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Double saldo = 110.20;
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite o número da sua conta");
+        String numero = scanner.next();//tipo string
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int agencia = scanner.nextInt();
+
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá  " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " conta " + numero + "e seu saldo " + saldo + " já está disponível para saque." );
+       
+    }
+}
